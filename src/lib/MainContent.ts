760b77bde9b81a4b0cedaf2bfacddd9dbaf6df4e
amay077 <okuokuoku@gmail.com>
2024-05-27 00:43:17 +0900

    gh

--- src/lib/MainContent.ts
+++ src/lib/MainContent.ts
@@ -218,29 +218,107 @@ const postToBluesky = async (text: string): Promise<boolean> => {
   }
 }; 
 
+const uploadImage = async (content: string /*file: File*/): Promise<string | undefined> => {
+  const settings: { 
+    branch_name: string,
+    path_to_images: string,
+    github_access_token: string,
+    repository_owner: string,
+    repository_name: string,
+  } = {
+    branch_name: 'main',
+    path_to_images: 'src/img',
+    github_access_token: '<token>',
+    repository_owner: 'amay077',
+    repository_name: 'blog2023',
+  }
+
+  const fileToBase64 = (file: File): Promise<string> => {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      console.log('readAsDataURL start', new Date().getTime());
+      reader.readAsDataURL(file);
+      reader.onload = (r) => {
+        console.log('readAsDataURL end', new Date().getTime());
+        console.log('replace start', new Date().getTime());
+        const prefix = `data:${file.type}:base64,`;
+        const base64str = (r.target?.result as string).substring(prefix.length);
+        console.log('replace end', new Date().getTime());
+        resolve(base64str);
+      };
+      reader.onerror = (e) => reject(e);
+    });
+  };
+
+  // console.log('fileToBase64 start', new Date().getTime());
+  // const content = await fileToBase64(file);
+  // console.log('fileToBase64 end', new Date().getTime());
+
+  const data = JSON.stringify({
+    'branch': settings.branch_name,
+    'message': 'upload image via POSTEIRO',
+    'content': `${content}`
+  });
+
+  const token = settings.github_access_token;
+  const owner = settings.repository_owner;
+  const repo = settings.repository_name;
+
+  // const extPos = file.name?.lastIndexOf('.') + 1 ?? -1;
+  // const ext = extPos > 0 ? file.name.substring(extPos) : 'png';
+  const ext = 'png';
+  const date = new Date().toISOString();
+  const url = `https://api.github.com/repos/${owner}/${repo}/contents/${settings.path_to_images}/${date}.${ext}`;
+
+  const p = {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      // 'Content-Type': 'image/png' //file.type
+    },
+    body: data
+  };
+
+  const res = await fetch(url, p);
+  if (res.ok) {
+    const resJson = await res.json();
+    console.log(`doUpload ~ resJson`, resJson, resJson.content.download_url);
+
+    return resJson.content.download_url;
+  } else {
+    return undefined;
+  }
+}
+
 const postToTwritter = async (text: string, images: string[]): Promise<boolean> => {
   try {
-    const settings = postSettings.twitter!;
-    const token = settings.token_data.token;
+    // const settings = postSettings.twitter!;
+    // const token = settings.token_data.token;
+
+    for (const dataURI of images) {
+      const image = dataURI.split(',')[1]
+      await uploadImage(image);
+    }
+
     /*
     const images: string[] = [
       'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII',
     ];
     */
-    const res = await fetch(`${Config.API_ENDPOINT}/twitter_post`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/plain',
-      },
-      body: JSON.stringify({ token, text, images }),
-    });
+    // const res = await fetch(`${Config.API_ENDPOINT}/twitter_post`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'text/plain',
+    //   },
+    //   body: JSON.stringify({ token, text, images }),
+    // });
 
-    if (res.ok) {
-      const resJson = await res.json();
-      console.log(`FIXME 後で消す  -> postToTwritter -> resJson:`, resJson);
-    } else {
-      return false;
-    }
+    // if (res.ok) {
+    //   const resJson = await res.json();
+    //   console.log(`FIXME 後で消す  -> postToTwritter -> resJson:`, resJson);
+    // } else {
+    //   return false;
+    // }
     return true;       
   } catch (error) {
     console.error(`postToMastodon -> error:`, error);
