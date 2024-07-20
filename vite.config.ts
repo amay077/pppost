20db8786fe2b07674f03754f11b4c9859979cafe
amay077 <okuokuoku@gmail.com>
2024-07-20 18:57:50 +0900

    #1 PWA化

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'  // これを追加
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  strategies: 'generateSW' as 'generateSW', // デフォルトなので不要
+  registerType: 'prompt' as 'prompt',       // デフォルトなので不要
+  manifest: {
+    name: 'PPPOST',
+    short_name: 'PPPOST',
+    description: 'Multi post for SNS',
+    icons: [
+      {
+        src: 'app.png',
+        sizes: '100x100',
+        type: 'image/png',
+      },
+    ],
+    start_url: 'index.html',
+    display: 'standalone',
+    background_color: '#ECFFE6',
+    theme_color: '#399918',
+    lang: 'ja',
+  },
+  // includeAssets: ['ui_icon/*.svg'],
+  // devOptions: {
+  //   enabled: true,
+  // },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [svelte()],
+  plugins: [svelte(), VitePWA(pwaOptions)],
 
   css: {
     preprocessorOptions: {
